ae061e58f58188c0a74b0c8d60e86b602f24742e
cc <412214410@qq.com>
2018-09-17 18:12:26 +0800

    knowledge extraction

--- file_knowledge/knowledge_extraction.py
+++ file_knowledge/knowledge_extraction.py
@@ -15,6 +15,7 @@ import sys
 from tool.db_connector import dbConnector
 from tool.logger import logger
 from tool.trans_dic import *
+import elasticsearch
 
 # GOLBAL PARAMS
 THUNLP_MODEL_PATH = "/home/showlove/cc/code/THULAC-Python/models"
@@ -25,6 +26,29 @@ MONGODB_PORT = 27017
 MONGODB_DB = "gov_finace"
 MONGODB_COLLECTION = "center"
 
+"""
+文本域
+es file format
+index   finace
+type    notice/file
+{
+    'publish_time': '',     # 发布时间
+    'publish_location':'',  # 发布地点
+    'publish_org': '',      # 发布机构
+    'publish_org_2': '',    # 发布的二级机构
+    'title': '',            # 标题
+    'category': '',         # 类别，notice [政策发布, 财经视点, 政策解读, 财政数据] file [文本附件，数据附件， 表格附件]
+    'content': '',          # 文本内容
+    'content_identify': '', # content的标识，例：财会〔2018〕24号
+    'content_attach': '',   # content的后缀
+    'quote_title': [],      # 标题引用文件
+    'quote_content':[],     # 正文引用文件
+    'entity_loc': [],       # 命名实体，地点
+    'entity_org': [],       # 明明是提，机构
+    'entity_name': [],      # 命名实体，姓名
+    'attachment_file': [],  # 附件列表
+}
+"""
 
 mongo = dbConnector(MONGODB_SERVER, MONGODB_PORT, MONGODB_DB, MONGODB_COLLECTION)
 for record in mongo.collection.find():
