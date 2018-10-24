e6b63218cdf0f74840c202f8f90a2f1c202b5584
cc <412214410@qq.com>
2018-10-24 16:10:24 +0800

    update text_rank_seg by merge model

--- tool/text_rank_seg.py
+++ tool/text_rank_seg.py
@@ -43,7 +43,8 @@ class TextSummary4Seg(object):
         self.thunlp_model = nlp_model
         # 分词/清洗
         if not doc_seg:
-            self.doc_seg_clear = self.cut_doc()
+            self.origin_doc_seg = self.cut_doc()
+            self.doc_seg_clear = self._clear_seg_list(self.origin_doc_seg)
         else:
             self.origin_doc_seg = self.doc
             self.doc_seg_clear = self._clear_seg_list(self.doc)
@@ -64,9 +65,9 @@ class TextSummary4Seg(object):
         #                          user_dict=THUNLP_USER_DIC_PATH)
         doc_seg = self.thunlp_model.cut(self.doc)
         # 保存原始分词结果，进行关键相邻词的短语组合
-        self.origin_doc_seg = doc_seg
-        doc_seg_clear = self._clear_seg_list(doc_seg)
-        return doc_seg_clear
+        # self.origin_doc_seg = doc_seg
+        # doc_seg_clear = self._clear_seg_list(doc_seg)
+        return doc_seg
 
     def _clear_seg_list(self, doc_seg):
         """
@@ -218,31 +219,71 @@ class TextSummary4Seg(object):
         result = sorted(word_imp.items(), key=lambda x:x[1], reverse=True)
         return result[:top_n]
 
+    def seg_merge(self, top_n=20):
+        """
+        排序靠前的seg，如果在文档中处于相邻位置则进行融合
+        :return:
+        """
+        _top_seg = self.top_n_seg(top_n=top_n)
+        top_seg = [seg[0] for seg in _top_seg]
+        top_merge_seg = list()
+        seg_index = list()
+        doc_length = len(self.origin_doc_seg_list)
+        for seg in top_seg:
+            seg_index += [index for index, _ in enumerate(self.origin_doc_seg_list) if _==seg]
+        seg_index = sorted(seg_index, key=lambda x:x, reverse=False)
+        # merge part
+        cache_list = list()
+        for n in seg_index:
+            if n not in cache_list:
+                merge_list = list()
+                merge_list.append(n)
+                for addition in range(1, top_n):
+                    if n + addition in seg_index:
+                       merge_list.append(n + addition)
+                    else:
+                        break
+                if len(merge_list) > 1:
+                    top_merge_seg.append(merge_list)
+                    cache_list += merge_list
+            else:
+                continue
+        # get merge string
+        result_list = list()
+        for _merge_list in top_merge_seg:
+            result_string = ''
+            for index in _merge_list:
+                result_string += self.origin_doc_seg_list[index]
+            result_list.append(result_string)
+        return result_list
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     doc = u'程序员(英文Programmer)是从事程序开发、维护的专业人员。一般将程序员分为程序设计人员和程序编码人员，但两者的界限并不非常清楚，特别是在中国。软件从业人员分为初级程序员、高级程序员、系统分析员和项目经理四大类。'
-    # doc = """
-    #     网易体育2月11日讯：^M
-    # 2007/2008赛季CBA联赛总决赛首回合比赛^M
-    # 将于北京时间2月13日晚7点半正式打响^M
-    # ，首场较量华南虎广东宏远将坐镇主场迎接东北虎辽宁盼盼的挑战，比赛打到这个份上，总
-    # 冠军奖杯近在咫尺，谁都不想遗憾地错过，本轮比赛，两只老虎势必会有一场殊死之战。^M
-    # 相对于篮球场上其它位置，大前锋在队上担任的任务几乎都是以苦工为主，要抢篮板、防守
-    # 、卡位都少不了他，但是要投篮、得分，他却经常是最后一个，从一定程度上说，大前锋是
-    # 篮球场上最不起眼的。但是就是这个位置，却往往在比赛中扮演着至关重要的角色。下面就
-    # 让我们来比较以下两队在这个位置上的人员配置。^M
-    # 广东队这个位置杜锋、朱芳雨都能独挡一面，即使在国内篮坛来说，这个人员储备都称得上
-    # 是豪华。辽宁队的刘相韬、李晓旭与谷立业就队内来说也是这个位置上的好手。但是把他们
-    # 放到一个同等的界面上来说，却又有很大的不同。^M
-    # 国内名气方面：^M
-    # 广东队无疑要远远胜于辽宁，无论是杜锋还是朱芳雨都是国字号球员，在国内篮坛都是赫赫
-    # 有名的角色，相比较而言，辽宁队的刘相韬，谷立业尽管在辽宁上有一些名气，但是在国内
-    # 篮坛他们还远称不上“大腕”。^M
-    # 个人技术方面：
-    # """
-    thunlp_model = thulac.thulac(seg_only=False, model_path=THUNLP_MODEL_PATH, \
-                                                                user_dict=THUNLP_USER_DIC_PATH)
+    doc = """
+        网易体育2月11日讯：^M
+    2007/2008赛季CBA联赛总决赛首回合比赛^M
+    将于北京时间2月13日晚7点半正式打响^M
+    ，首场较量华南虎广东宏远将坐镇主场迎接东北虎辽宁盼盼的挑战，比赛打到这个份上，总
+    冠军奖杯近在咫尺，谁都不想遗憾地错过，本轮比赛，两只老虎势必会有一场殊死之战。^M
+    相对于篮球场上其它位置，大前锋在队上担任的任务几乎都是以苦工为主，要抢篮板、防守
+    、卡位都少不了他，但是要投篮、得分，他却经常是最后一个，从一定程度上说，大前锋是
+    篮球场上最不起眼的。但是就是这个位置，却往往在比赛中扮演着至关重要的角色。下面就
+    让我们来比较以下两队在这个位置上的人员配置。^M
+    广东队这个位置杜锋、朱芳雨都能独挡一面，即使在国内篮坛来说，这个人员储备都称得上
+    是豪华。辽宁队的刘相韬、李晓旭与谷立业就队内来说也是这个位置上的好手。但是把他们
+    放到一个同等的界面上来说，却又有很大的不同。^M
+    国内名气方面：^M
+    广东队无疑要远远胜于辽宁，无论是杜锋还是朱芳雨都是国字号球员，在国内篮坛都是赫赫
+    有名的角色，相比较而言，辽宁队的刘相韬，谷立业尽管在辽宁上有一些名气，但是在国内
+    篮坛他们还远称不上“大腕”。^M
+    个人技术方面：
+    """
+    thunlp_model = thulac.thulac(seg_only=False, model_path=THUNLP_MODEL_PATH, user_dict=THUNLP_USER_DIC_PATH)
     model = TextSummary4Seg(doc, 6, 0.85, 700, thunlp_model)
-    model.print_result()
+    # model.print_result()
     # for seg in model.origin_doc_seg_list:
     #     print seg
+    top_n_seg = model.top_n_seg(10)
+    top_n_seg_merge = model.seg_merge(20)
+    pass
